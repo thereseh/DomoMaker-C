b2dc39baf8c0e142b6c9c3421d6e12546d2769da
Therese Henriksson <tah5279@rit.edu>
2017-04-06 20:03:12 -0400

    middleware

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ const expressHandlebars = require('express-handlebars');
 const session = require('express-session');
 const RedisStore = require('connect-redis')(session);
 const url = require('url');
+const csrf = require('csurf');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
@@ -38,6 +39,7 @@ const router = require('./router.js');
 const app = express();
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+app.disable('x-powered-by');
 app.use(compression());
 app.use(bodyParser.urlencoded({
   extended: true,
@@ -52,11 +54,22 @@ app.use(session({
   secret: 'Domo Arigato',
   resave: true,
   saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+  },
 }));
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
 app.use(cookieParser());
+app.use(csrf());
+app.use((err, req, res, next) => {
+  if (err.code !== 'EBADCRFTOKEN') return next(err);
+
+  console.log('Missing CSRF token');
+
+  return false;
+});
 
 router(app);
 
